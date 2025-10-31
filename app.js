fce65d4d9748324ef1b18c36a2e7c7fd948d9fb2
Ooha Bhargavi Yamasani <oohabhargaviyamasani@gmail.com>
2025-10-31 16:44:04 +0530

    Configured Vercel for Node.js Express deployment

--- app.js
+++ app.js
@@ -139,10 +139,11 @@ app.use((err, req, res, next)=>
     //res.status(statusCode).send(message);
 });
 
-app.listen(8080,()=>
-{
-    console.log("server is listening");
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+    console.log(`Server is listening on port ${PORT}`);
 });
+
 
 
 // app.get("/testListing",async(req,res)=>
